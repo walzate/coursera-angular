5f0f2fed2c820f42c6c70b305ff789f6556e0bd3
Wilson Alzate-Calderon <wilson.alzate@gmail.com>
2016-10-16 20:12:12 -0500

    All but the directive

--- module3-solution/javascript/app.js
+++ module3-solution/javascript/app.js
@@ -4,26 +4,93 @@
 
 angular.module('NarrowItDownApp',[])
 .controller('NarrowItDownController', NarrowItDownController )
-.service('MenuSearchService', MenuSearchService);
+.service('MenuSearchService', MenuSearchService)
+/*.directive('foundItems', FoundItemsDirective);
+
+function FoundItemsDirective() {
+  var ddo = {
+    templateUrl: 'loader/itemsloaderindicator.template.html',
+    scope: {
+      items: '<',
+      myTitle: '@title',
+      badRemove: '=',
+      onRemove: '&'
+    },
+    controller: FoundItemsDirectiveController,
+    controllerAs: 'list',
+    bindToController: true
+  };
+
+  return ddo;
+}
+
+function FoundItemsDirectiveController() {
+  var list = this;
+
+  list.cookiesInList = function () {
+    for (var i = 0; i < list.items.length; i++) {
+      var name = list.items[i].name;
+      if (name.toLowerCase().indexOf("cookie") !== -1) {
+        return true;
+      }
+    }
+
+    return false;
+  };
+}*/
 
 // Properly inject $scope into the controller using the $inject property
 // (shown how in video lecture) to make sure to protect your code from minification.
 NarrowItDownController.$inject = ["MenuSearchService"];
 function NarrowItDownController(MenuSearchService){
+  var narrow = this;
+  narrow.searchTerm="";
+
+  narrow.getMatchedMenuItems = function (){
+    MenuSearchService.getMatchedMenuItems(narrow.searchTerm);
+    narrow.foundItems = MenuSearchService.getFoundItems();
+  };
+
+  narrow.removeItem = function(index){
+    MenuSearchService.removeItem(index);
+  }
 
 };
 
-function MenuSearchService(){
+MenuSearchService.$inject = ['$http']
+function MenuSearchService($http){
   var service = this;
+  var foundItems = [];
 
   service.getMatchedMenuItems = function (searchTerm){
-    //return $http(...).then(function (result) {
-        // process result and only keep items that match
-        //var foundItems...
+    foundItems = [];
 
-        // return processed items
-        //return foundItems;
-    //});
+    var promise = $http({
+      method: "GET",
+      url: "https://davids-restaurant.herokuapp.com/menu_items.json"
+    }).then(function (result) {
+      if(searchTerm.length>0){
+        var menuItems = result.data.menu_items;
+        var i=0;
+        for (var i = 0; i<menuItems.length ; i++){
+          item = menuItems[i];
+          if(item.description.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1){
+            foundItems.push(item);
+          }
+          i++;
+        }
+      }
+    });;
+
+    return promise;
   }
-}
+
+  service.getFoundItems = function (){
+    return foundItems;
+  }
+
+  service.removeItem = function (index){
+    foundItems.splice(index, 1);
+  }
+};
 })();
